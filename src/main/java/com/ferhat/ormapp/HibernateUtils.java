72f6b30067341fc19e27be397faa0e0743480c5a
ferhatBedir <38124974+ferhatBedir@users.noreply.github.com>
2018-04-09 21:22:20 +0300

    created file

---
+++ src/main/java/com/ferhat/ormapp/HibernateUtils.java
@@ -0,0 +1,31 @@
+package com.ferhat.ormapp;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.service.ServiceRegistry;
+
+public class HibernateUtils {
+
+    private static final SessionFactory sessionFactory = buidSessionFactory();
+
+    private static SessionFactory buidSessionFactory() {
+        try {
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+            Metadata metadata = new MetadataSources(serviceRegistry).getMetadataBuilder().build();
+            return metadata.getSessionFactoryBuilder().build();
+        } catch (Throwable ex) {
+            System.out.println("Session Failed" + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public static void shutdown() {
+        getSessionFactory().close();
+    }
+}
