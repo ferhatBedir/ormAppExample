2e758d2840e48a827c3bdaadd84093cf2d2f7199
ferhatBedir <38124974+ferhatBedir@users.noreply.github.com>
2018-04-09 20:14:47 +0300

    created file

---
+++ src/main/java/com/ferhat/ormapp/Timekeeper.java
@@ -0,0 +1,62 @@
+package com.ferhat.ormapp;
+
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "TIMEKEEPER")
+
+public class Timekeeper {
+
+    public static final char IN = 'I';
+    public static final char OUT = 'O';
+    private String timeKeeperId;
+    private Date dateTime;
+    private Employee employee;
+    private char inOut;
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    @Column(name = "Timekeeper_Id", length = 36)
+    public String getTimeKeeperId() {
+        return timeKeeperId;
+    }
+
+    public void setTimeKeeperId(String timeKeeperId) {
+        this.timeKeeperId = timeKeeperId;
+    }
+
+    @Column(name="Date_Time",nullable = false)
+    @Temporal(TemporalType.DATE.TIMESTAMP)
+    public Date getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(Date dateTime) {
+        this.dateTime = dateTime;
+    }
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="EMP_ID",nullable = false)
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    @Column(name="In_Out", nullable = false,length = 1)
+    public char getInOut() {
+        return inOut;
+    }
+
+    public void setInOut(char inOut) {
+        this.inOut = inOut;
+    }
+}
