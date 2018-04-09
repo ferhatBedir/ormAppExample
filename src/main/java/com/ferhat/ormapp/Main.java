2435e8f96fd5e9c5752e91fcee23f368fc22f2eb
ferhatBedir <38124974+ferhatBedir@users.noreply.github.com>
2018-04-09 21:21:03 +0300

    created file

--- src/main/java/com/ferhat/ormapp/Main.java
+++ src/main/java/com/ferhat/ormapp/Main.java
@@ -1,7 +1,34 @@
 package com.ferhat.ormapp;
 
+import java.util.List;
+
+import org.hibernate.query.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
 public class Main {
-    public static void main(String [] args){
+    public static void main(String[] args) {
+
+        SessionFactory factory = HibernateUtils.getSessionFactory();
+        Session session = factory.getCurrentSession();
+
+        try {
+            session.getTransaction().begin();
+            String sql = "Select e from" + Employee.class.getName() + "e" + "order by e.empName, e.empNo";
+            Query<Employee> query = session.createQuery(sql);
+
+            List<Employee> emloyees = query.getResultList();
+
+            for (Employee emp : emloyees) {
+                System.out.println("Emp: " + emp.getEmpName() + ":" + emp.getEmpName());
+
+                session.getTransaction().commit();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            session.getTransaction().rollback();
+        }
+
 
     }
 }
